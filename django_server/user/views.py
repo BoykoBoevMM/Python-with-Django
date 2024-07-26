ab363bdf6d7e735f95823b44940b4aa88e192232
Boyko Boev <boyko.boev@mentormate.com>
2024-07-26 18:42:59 +0300

    Add base html structure and styles

--- django_server/user/views.py
+++ django_server/user/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 
 def home(request):
-    return render(request, 'user/home.html')
+    posts = [
+        {
+            'author': 'Author Name',
+            'title': 'First Blog Post',
+            'content': 'First post content',
+            'date': datetime.date.today(),
+        }, {
+            'author': 'Author Name',
+            'title': 'First Blog Post',
+            'content': 'First post content',
+            'date': datetime.date.today(),
+        }
+    ]
+    context = {
+        'posts': posts
+    }
+    return render(request, 'user/home.html', context)
     # return HttpResponse("<h2>Home page</h2>")
 
 def login(request):
-    return render(request, 'user/login.html')
+    context = {
+        'title': 'Login' 
+    }
+    return render(request, 'user/login.html', context)
     # return HttpResponse("<h2>Login page</h2>")
 
 def register(request):
-    return render(request, 'user/register.html')
+    context = {
+        'title': 'Register' 
+    }
+    return render(request, 'user/register.html', context)
     # return HttpResponse("<h2>Register page</h2>")
