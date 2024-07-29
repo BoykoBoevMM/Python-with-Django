5b1d6e0431cc01033bb39966954fe7e2301be35d
Boyko Boev <boyko.boev@mentormate.com>
2024-07-29 14:47:32 +0300

    Create post model with migrations and database
    Migrations and database

--- django_server/user/views.py
+++ django_server/user/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 import datetime
 
 
 def home(request):
-    posts = [
-        {
-            'author': 'Author Name',
-            'title': 'First Blog Post',
-            'content': 'First post content',
-            'date': datetime.date.today(),
-        }, {
-            'author': 'Author Name',
-            'title': 'First Blog Post',
-            'content': 'First post content',
-            'date': datetime.date.today(),
-        }
-    ]
+    # posts = [
+    #     {
+    #         'author': 'Author Name',
+    #         'title': 'First Blog Post',
+    #         'content': 'First post content',
+    #         'date': datetime.date.today(),
+    #     }, {
+    #         'author': 'Author Name',
+    #         'title': 'First Blog Post',
+    #         'content': 'First post content',
+    #         'date': datetime.date.today(),
+    #     }
+    # ]
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'user/home.html', context)
     # return HttpResponse("<h2>Home page</h2>")
