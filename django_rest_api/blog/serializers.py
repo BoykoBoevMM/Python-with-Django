653913a08be511a05a59ded4c8a7b418b2086748
Boyko Boev <boyko.boev@mentormate.com>
2024-08-12 15:25:15 +0300

    Add comments permissions

--- django_rest_api/blog/serializers.py
+++ django_rest_api/blog/serializers.py
@@ -7,12 +7,26 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+        # fields = ['title', 'content', 'link', 'tags']
+        read_only_fields = ['id', 'author', 'date']
+        
+    def create(self, validated_data):
+        request = self.context.get('request', None)
+        post = Post.objects.create(author=request.user, **validated_data)
+        return post
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
+        # fields = ['content']
         fields = '__all__'
+        read_only_fields = ['id', 'author', 'date', 'post']
+    
+    def create(self, validated_data):
+        request = self.context.get('request', None)
+        comment = Comment.objects.create(author=request.user, **validated_data)
+        return comment
 
 
 class TagSerializer(serializers.ModelSerializer):
