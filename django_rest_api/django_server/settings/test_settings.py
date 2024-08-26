83980bceebccea9930ada49bed317c383c1eb97b
Boyko Boev <boyko.boev@mentormate.com>
2024-08-26 15:30:15 +0300

    Resolve PR comments

--- django_rest_api/django_server/settings/test_settings.py
+++ django_rest_api/django_server/settings/test_settings.py
@@ -1,23 +1,5 @@
-import os
 from .base_settings import *
-from dotenv import load_dotenv
-
-dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), '.env')
-load_dotenv(dotenv_path=dotenv_path)
 
 DEBUG=False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
-
-SECRET_KEY=os.getenv('SECRET_KEY')
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('TEST_DATABASE_NAME'),
-        'USER': os.getenv('TEST_DATABASE_USER'),
-        'PASSWORD': os.getenv('TEST_DATABASE_PASSWORD'),
-        'HOST': os.getenv('TEST_DATABASE_HOST', 'localhost'),
-        'PORT': os.getenv('TEST_DATABASE_PORT', 5433),
-    }
-}
