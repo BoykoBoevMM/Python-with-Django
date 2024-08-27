d61fcff7294041df5a4cbbea5382e9824981eee4
Boyko Boev <boyko.boev@mentormate.com>
2024-08-27 17:58:04 +0300

    Update pytest fixtures and tests

--- django_rest_api/blog/tests.py
+++ django_rest_api/blog/tests.py
@@ -1,26 +1,11 @@
 import pytest
 import json
-from django.test import TestCase
 from django.urls import reverse
-from django.utils import timezone
-
 from rest_framework import status
 from rest_framework.exceptions import ErrorDetail
-
-from .models import Post, Tag
-from user.models import CustomUser
-from datetime import datetime
+from .models import Post
 
 
-email ='test@test.com'
-username ='TestTest'
-password ='1234567890'
-user_data = {
-    "id": 1,
-    "email": email,
-    "username": username,
-    "password": password,
-}
 tag_data = {
     "name": "pytest"
 }
@@ -30,54 +15,20 @@ post_data = {
     "tags": [tag_data]
 }
 
-@pytest.fixture
-def user():
-    return CustomUser.objects.create_user(**user_data)
-
-def login_user(client):
-    response = client.post(reverse('user-login'), user_data)
-    token = response.data["token"]
-    return token
-
-
-@pytest.fixture
-def post():
-    user_data = CustomUser.objects.get(
-        username=username, 
-        email=email
-    )
-    post1 = Post.objects.create(
-        title=post_data['title'], 
-        content=post_data['content'], 
-        author=user_data,
-        date=timezone.make_aware(datetime(2024, 1, 1))
-    )
-    post2 = Post.objects.create(
-        title=post_data['title'], 
-        content=post_data['content'], 
-        author=user_data,
-        date=timezone.make_aware(datetime(2024, 2, 2))
-    )
-    tag = Tag.objects.create(**tag_data)
-    post1.tags.set([tag])
-    return [post1, post2]
-
 
 POST_LIST_URL = reverse('blog-home-list')
 POST_DETAIL_URL = reverse('blog-home-detail', kwargs={'pk': 1})
 
 
 class TestBlogPostOrdering:
-    @pytest.mark.django_db
-    def test_posts_list_ascending_order(self, client, user, post):
+    def test_posts_list_ascending_order(self, client, create_posts):
         ordering = 'date'
         response = client.get(f"{POST_LIST_URL}?ordering={ordering}")
         assert response.status_code == status.HTTP_200_OK
         assert response.data['count'] == 2
         assert response.data['results'][0]['date'] < response.data['results'][1]['date']
 
-    @pytest.mark.django_db
-    def test_posts_list_descending_order(self, client, user, post):
+    def test_posts_list_descending_order(self, client, create_posts):
         ordering = '-date'
         response = client.get(f"{POST_LIST_URL}?ordering={ordering}")
         assert response.status_code == status.HTTP_200_OK
@@ -86,30 +37,25 @@ class TestBlogPostOrdering:
 
 
 class TestCreateBlogPost:
-    @pytest.mark.django_db
-    def test_create_blog_post_unauthorized(self, client, user, post):
+    def test_create_blog_post_unauthorized(self, client, db):
         response = client.post(POST_LIST_URL, post_data)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
-        assert Post.objects.count() == 2
+        assert Post.objects.count() == 0
         assert response.data['detail'] == "Authentication credentials were not provided."
 
-    @pytest.mark.django_db
-    def test_create_blog_post_validation(self, client, user, post):
+    def test_create_blog_post_validation(self, client, create_token):
         validation = ErrorDetail(
             string='This field is required.', 
             code='required'
         )
-        token = login_user(client)
-        response = client.post(POST_LIST_URL, HTTP_AUTHORIZATION=f'Token {token}')
+        response = client.post(POST_LIST_URL, HTTP_AUTHORIZATION=f'Token {create_token}')
         assert response.status_code == status.HTTP_400_BAD_REQUEST
-        assert Post.objects.count() == 2
+        assert Post.objects.count() == 0
         assert response.data['title'] == [validation]
         assert response.data['content'] == [validation]
 
-    @pytest.mark.django_db
-    def test_create_blog_post(self, client, user):
-        token = login_user(client)
-        response = client.post(POST_LIST_URL, post_data, HTTP_AUTHORIZATION=f'Token {token}')
+    def test_create_blog_post(self, client, create_token):
+        response = client.post(POST_LIST_URL, post_data, HTTP_AUTHORIZATION=f'Token {create_token}')
         assert response.status_code == status.HTTP_201_CREATED
         assert Post.objects.count() == 1
         assert response.data['title'] == post_data['title']
@@ -117,58 +63,51 @@ class TestCreateBlogPost:
 
 
 class TestBlogPostDetails:
-    @pytest.mark.django_db
-    def test_blog_post_detail(self, client, user, post):
-        first_post = Post.objects.first()
+    def test_blog_post_detail(self, client, create_posts):
+        first_post = create_posts[0]
         POST_DETAIL_URL = reverse('blog-home-detail', kwargs={'pk': first_post.id})
         response = client.get(POST_DETAIL_URL)
         assert response.status_code == status.HTTP_200_OK
         assert response.data['title'] == post_data['title']
         assert response.data['content'] == post_data['content']
 
-    @pytest.mark.django_db
-    def test_blog_post_detail_unauthorized(self, client, user, post):
-        first_post = Post.objects.first()
+    def test_blog_post_detail_unauthorized(self, client, create_posts):
+        first_post = create_posts[0]
         POST_DETAIL_URL = reverse('blog-home-detail', kwargs={'pk': first_post.id})
         response = client.put(POST_DETAIL_URL, post_data)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
         assert Post.objects.count() == 2
         assert response.data['detail'] == "Authentication credentials were not provided."
 
-    @pytest.mark.django_db
-    def test_blog_post_detail_validation(self, client, user, post):
-        first_post = Post.objects.first()
+    def test_blog_post_detail_validation(self, client, create_token, create_posts):
+        first_post = create_posts[0]
         POST_DETAIL_URL = reverse('blog-home-detail', kwargs={'pk': first_post.id})
         validation = ErrorDetail(
             string='This field is required.', 
             code='required'
         )
-        token = login_user(client)
-        response = client.put(POST_DETAIL_URL, HTTP_AUTHORIZATION=f'Token {token}')
+        response = client.put(POST_DETAIL_URL, HTTP_AUTHORIZATION=f'Token {create_token}')
         assert Post.objects.count() == 2
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] == [validation]
         assert response.data['content'] == [validation]
 
-    @pytest.mark.django_db
-    def test_update_blog_post_detail(self, client, user, post):
-        first_post = Post.objects.first()
+    def test_update_blog_post_detail(self, client, create_token, create_posts):
+        first_post = create_posts[0]
         POST_DETAIL_URL = reverse('blog-home-detail', kwargs={'pk': first_post.id})
-        token = login_user(client)
         response = client.put(
             POST_DETAIL_URL,
             data=json.dumps(post_data),
             content_type='application/json', 
-            HTTP_AUTHORIZATION=f'Token {token}'
+            HTTP_AUTHORIZATION=f'Token {create_token}'
         )
         assert Post.objects.count() == 2
         assert response.status_code == status.HTTP_200_OK
         assert response.data['title'] == post_data['title']
         assert response.data['content'] == post_data['content']
 
-    @pytest.mark.django_db
-    def test_delete_blog_post_detail_validation(self, client, user, post):
-        first_post = Post.objects.first()
+    def test_delete_blog_post_detail_validation(self, client, create_posts):
+        first_post = create_posts[0]
         POST_DETAIL_URL = reverse('blog-home-detail', kwargs={'pk': first_post.id})
         assert Post.objects.count() == 2
         response = client.delete(POST_DETAIL_URL)
--- django_rest_api/user/tests.py
+++ django_rest_api/user/tests.py
@@ -1,6 +1,5 @@
 import pytest
 from django.urls import reverse
-from django.test import TestCase
 from rest_framework import status
 from rest_framework.exceptions import ErrorDetail
 from user.models import CustomUser
@@ -25,7 +24,7 @@ user = {
 }
 
 
-class UserRegisterTests(TestCase):
+class TestUserRegister:
     url = reverse('user-register')
     validation = {
         "required": ErrorDetail(
@@ -42,9 +41,8 @@ class UserRegisterTests(TestCase):
         )
     } 
 
-    @pytest.mark.django_db
-    def test_user_register_validation(self):
-        response = self.client.post(self.url)
+    def test_user_register_validation(self, client, db):
+        response = client.post(self.url)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert CustomUser.objects.count() == 0
         assert response.data['email'] == [self.validation["required"]]
@@ -52,26 +50,24 @@ class UserRegisterTests(TestCase):
         assert response.data['password1'] == [self.validation["required"]]
         assert response.data['password2'] == [self.validation["required"]]
 
-    @pytest.mark.django_db
-    def test_user_register_created(self):
-        response = self.client.post(self.url, user_register)
+    def test_user_register_created(self, client, db):
+        response = client.post(self.url, user_register)
         assert response.status_code == status.HTTP_201_CREATED
         assert CustomUser.objects.count() == 1
         assert response.data['id'] == user_register['id']
         assert response.data['username'] == user_register['username']
         assert type(response.data['token']) is str
 
-    @pytest.mark.django_db
-    def test_user_register_already_exist(self):
+    def test_user_register_already_exist(self, client, db):
         CustomUser.objects.create_user(**user)    
-        response = self.client.post(self.url, user_register)
+        response = client.post(self.url, user_register)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert CustomUser.objects.count() == 1
         assert response.data['email'] == [self.validation["email"]]
         assert response.data['username'] == [self.validation["username"]]
 
 
-class UserLoginTests(TestCase):
+class TestUserLogin:
     url = reverse('user-login')
     validation = {
         "authentication_failed": ErrorDetail(
@@ -88,46 +84,39 @@ class UserLoginTests(TestCase):
         )
     } 
 
-    @pytest.mark.django_db
-    def test_user_login_empty_data(self):
-        response = self.client.post(self.url)
+    def test_user_login_empty_data(self, client, db):
+        response = client.post(self.url)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['username'] == [self.validation["username"]]
         assert response.data['password'] == [self.validation["password"]]
 
-    @pytest.mark.django_db
-    def test_user_login_validation(self):
-        response = self.client.post(self.url, user)
+    def test_user_login_validation(self, client, db):
+        response = client.post(self.url, user)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
         assert response.data['detail'] == self.validation["authentication_failed"]
     
-    @pytest.mark.django_db
-    def test_user_login(self):
+    def test_user_login(self, client, db):
         CustomUser.objects.create_user(**user)
-        response = self.client.post(self.url, user)
+        response = client.post(self.url, user)
         assert response.status_code == status.HTTP_200_OK
         assert response.data['id'] == user["id"]
         assert response.data['username'] == user["username"]
         assert type(response.data['token']) is str
 
 
-class UserLogoutTests(TestCase):
+class TestUserLogout:
     url = reverse('user-logout')
     
-    @pytest.mark.django_db
-    def test_user_logout_empty_data(self):
+    def test_user_logout_empty_data(self, client, db):
         validation = ErrorDetail(
             string='Authentication credentials were not provided.', 
             code='not_authenticated'
         )
-        response = self.client.post(self.url)
+        response = client.post(self.url)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
         assert response.data["detail"] == validation
 
-    @pytest.mark.django_db
-    def test_user_logout(self):
-        response = self.client.post(reverse('user-register'), user_register)
-        token = response.data["token"]
-        response = self.client.post(self.url, HTTP_AUTHORIZATION=f'Token {token}')
+    def test_user_logout(self, client, db, create_token):
+        response = client.post(self.url, HTTP_AUTHORIZATION=f'Token {create_token}')
         assert response.status_code == status.HTTP_200_OK
         assert response.data['detail'] == "Logged out successfully"
